02fe74b7eb1fa6948534eb6b39307a5c60a57d62
P17Seongbin <sungbin0515@postech.ac.kr>
2019-11-14 16:54:41 +0900

    Modify dependency

--- team4_codecompletion_plugin/src/main/java/PostfixCompletion/Template/IfPostfixTemplate.java
+++ team4_codecompletion_plugin/src/main/java/PostfixCompletion/Template/IfPostfixTemplate.java
@@ -4,7 +4,12 @@ import com.intellij.codeInsight.template.postfix.templates.PostfixTemplate;
 import com.intellij.codeInsight.template.postfix.templates.PostfixTemplateProvider;
 import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.Editor;
-import com.intellij.psi.PsiElement;
+import com.intellij.psi.*;
+import com.intellij.ide.projectView.impl.nodes.PackageUtil;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.roots.ProjectRootManager;
+import com.intellij.psi.*;
+import com.thaiopensource.xml.dtd.om.Def;
 
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -24,6 +29,7 @@ public class IfPostfixTemplate extends PostfixTemplate {
     @Override
     public void expand(@NotNull PsiElement context, @NotNull Editor editor) {
         //TODO: IMplement this
+        
     }
 }
 
