bb04cd7375919d6b9607fdae80b4292e8f6e8958
P17Seongbin <sungbin0515@postech.ac.kr>
2019-11-14 16:38:22 +0900

    Implement skeleton code of IfPostfixTemplate

--- team4_codecompletion_plugin/src/main/java/PostfixCompletion/Template/IfPostfixTemplate.java
+++ team4_codecompletion_plugin/src/main/java/PostfixCompletion/Template/IfPostfixTemplate.java
@@ -1,4 +1,30 @@
 package PostfixCompletion.Template;
 
-public class IfPostfixTemplate {
+import com.intellij.codeInsight.template.postfix.templates.PostfixTemplate;
+import com.intellij.codeInsight.template.postfix.templates.PostfixTemplateProvider;
+import com.intellij.openapi.editor.Document;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.psi.PsiElement;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class IfPostfixTemplate extends PostfixTemplate {
+    protected IfPostfixTemplate()
+    {
+        super("if","Make default if structure","if([statement]) {}");
+    }
+
+    @Override
+    public boolean isApplicable(@NotNull PsiElement context, @NotNull Document copyDocument, int newOffset) {
+        //TODO: Implement this
+        return true;
+    }
+
+    @Override
+    public void expand(@NotNull PsiElement context, @NotNull Editor editor) {
+        //TODO: IMplement this
+    }
 }
+
+p
