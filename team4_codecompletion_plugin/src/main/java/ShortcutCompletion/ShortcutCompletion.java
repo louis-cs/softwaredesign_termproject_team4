ab2a18337d3e0b567c2b902385c7f6456af33f02
KEECHANG CHOI <keechang@postech.ac.kr>
2019-11-18 03:44:34 +0900

    shortcut_c

---
+++ team4_codecompletion_plugin/src/main/java/ShortcutCompletion/ShortcutCompletion.java
@@ -0,0 +1,22 @@
+package ShortcutCompletion;
+
+import com.intellij.codeInsight.completion.*;
+import com.intellij.codeInsight.lookup.LookupElementBuilder;
+import com.intellij.patterns.PlatformPatterns;
+import com.intellij.util.ProcessingContext;
+import org.jetbrains.annotations.NotNull;
+
+public class ShortcutCompletion extends CompletionContributor {
+    public ShortcutCompletion() {
+        extend(CompletionType.BASIC,
+                PlatformPatterns.psiElement(),
+                new CompletionProvider<CompletionParameters>() {
+                    public void addCompletions(@NotNull CompletionParameters parameters,
+                                               ProcessingContext context,
+                                               @NotNull CompletionResultSet resultSet) {
+                        resultSet.addElement(LookupElementBuilder.create("SHORTCUT@@@@@@@@@@"));
+                    }
+                }
+        );
+    }
+}
