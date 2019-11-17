d3948614986ceecea1785acd742de948db1b2e3e
P17Seongbin <sungbin0515@postech.ac.kr>
2019-11-17 15:57:35 +0900

    Override all method of CompletionContributor

--- team4_codecompletion_plugin/src/main/java/ArgumentCompletion/ArgumentCompletionContributor.java
+++ team4_codecompletion_plugin/src/main/java/ArgumentCompletion/ArgumentCompletionContributor.java
@@ -1,8 +1,7 @@
 package ArgumentCompletion;
 
-import com.intellij.codeInsight.completion.CompletionContributor;
-import com.intellij.codeInsight.completion.CompletionParameters;
-import com.intellij.codeInsight.completion.CompletionResultSet;
+import com.intellij.codeInsight.completion.*;
+import com.intellij.openapi.editor.Editor;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -10,14 +9,46 @@ import org.jetbrains.annotations.Nullable;
  * Custom Completion Contributor
  * : to add condition to show popup. (condition: 'return ' typed)
  */
-
+//BeforeCompletion -> DuringCOmpletion -> FillCompletion  순으로 호출되는거 같음
 public class ArgumentCompletionContributor extends CompletionContributor {
+
+    public ArgumentCompletionContributor() {
+
+        super();
+        System.out.println("Creation");
+    }
 
     @Override
     public void fillCompletionVariants(@NotNull CompletionParameters parameters, @NotNull CompletionResultSet result) {
+     System.out.println("FillCompletion");
         result.addLookupAdvertisement("Argument Completion from Team G4!");
-
         super.fillCompletionVariants(parameters, result);
+    }
+
+    @Override
+    public void beforeCompletion(@NotNull CompletionInitializationContext context) {
+        System.out.println("Before");
+        super.beforeCompletion(context);
+    }
+
+    @Nullable
+    @Override
+    public String handleEmptyLookup(@NotNull CompletionParameters parameters, Editor editor) {
+        System.out.println("HandleEmptyLookup");
+        return super.handleEmptyLookup(parameters, editor);
+    }
+
+    @Nullable
+    @Override
+    public AutoCompletionDecision handleAutoCompletionPossibility(@NotNull AutoCompletionContext context) {
+        System.out.println("Text");
+        return super.handleAutoCompletionPossibility(context);
+    }
+
+    @Override
+    public void duringCompletion(@NotNull CompletionInitializationContext context) {
+        System.out.println("duringCompletion");
+        super.duringCompletion(context);
     }
 }
 
