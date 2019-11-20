8ab7cf8c5d20bd312f6ea3a3e8763b8180c1e99b
lchauvet <lchauvet@postech.ac.kr>
2019-11-20 18:10:50 +0900

    made things pretty

--- team4_codecompletion_plugin/src/main/java/CheckboxAction.java
+++ team4_codecompletion_plugin/src/main/java/CheckboxAction.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
+import javax.swing.*;
+import java.awt.event.ActionEvent;
 
-public class CheckboxAction {
+class EnableDisableCheckboxAction extends AbstractAction {
+    public EnableDisableCheckboxAction(String text) {
+        super(text);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        JCheckBox cbLog = (JCheckBox) e.getSource();
+        if (cbLog.isSelected()) {
+            System.out.println("Feature enabled!");
+            //enable the feature somehow
+        } else {
+            System.out.println("Feature disabled :(");
+            //disable the feature
+        }
+    }
+
 }
